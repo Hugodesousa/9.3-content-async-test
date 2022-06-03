42ad997913bc03b4f1c374ca166fe2af77cf1052
Hugodesousa <hugodesousa.16@gmail.com>
2022-06-03 15:17:06 -0300

    exercicios

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,18 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Verifica nome do personagem', async () => {
+    const character = await fetchCharacter('720');
+    expect(character.name).toBe('Wonder Woman')
+  });
+  it('Verifica o erro caso a função não receba parametro', async () => {
+    const failRequest = await fetchCharacter();
+    console.log(failRequest)
+    expect(failRequest).toEqual(new Error('You must provide an url'));
+  });
+  it('Verifica se retorna \'Invalid id\' ao executar a função com parâmetro que não existe', async () => {
+    const response = await fetchCharacter('qlq');
+    
+    expect(response).toBe('Invalid id')
+  });
 });
